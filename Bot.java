5a592c947a27a35fb70803fc56b4ed1c8574d657
Marco Pretorius <mpre8@student.monash.edu>
2018-03-10 17:18:15 +1100

    trying to fix

--- Bot.java
+++ Bot.java
@@ -43,40 +43,50 @@ public class Bot
                     switch (reply.toUpperCase()) {
                         case "ACK 7":
                             currentBondBuyLists += 30;
+                            System.out.println("BOND Buy ACK");
                             break;
 
                         case "ACK 77":
                             currentBondSellLists += 30;
+                            System.out.println("BOND sell ACK");
                             break;
 
                         case "ACK 2":
                             currentVALEBuyLists += 2;
+                            System.out.println("Vale Buy ACK");
                             break;
 
 
                         case "ACK 3":
                             currentGSBuyLists += 2;
+                            System.out.println("GS Buy ACK");
                             break;
                         case "ACK 33":
                             currentGSSellLists += 2;
+                            System.out.println("GS Sell ACK");
 
                         case "ACK 4":
                             currentMSBuyLists += 2;
+                            System.out.println("MS Buy ACK");
                             break;
 
                         case "ACK 44":
                             currentMSSellLists += 2;
+                            System.out.println("MS Sell ACK");
                             break;
 
                         case "OUT 7":
                             currentBondBuyLists = 0;
+                            System.out.println("BOND BUY OUT");
                             break;
 
                         case "OUT 77":
                             currentBondSellLists = 0;
+                            System.out.println("BOND SELL OUT");
                             break;
                         case "OUT 2":
                             currentVALEBuyLists = 0;
+                            System.out.println("VALE Buy OUT");
                             totalVALE += 2;
                             if (totalVALE >= 10) {
                                 to_exchange.println(("CONVERT 10 VALE BUY 10").toUpperCase());
@@ -85,17 +95,21 @@ public class Bot
 
                         case "OUT 3":
                             currentGSBuyLists = 0;
+                            System.out.println("GS Buy OUT");
                             break;
 
                         case "OUT 33":
                             currentGSSellLists = 0;
+                            System.out.println("GS Sell OUT");
                             break;
 
                         case "OUT 4":
                             currentMSBuyLists = 0;
+                            System.out.println("MS Buy OUT");
                             break;
                         case "OUT 44":
                             currentMSSellLists = 0;
+                            System.out.println("MS Sell OUT");
                             break;
 
                         case "REJECT 77 DUPLICATE_ORDER_ID":
@@ -117,7 +131,7 @@ public class Bot
                         case "BOOK VALE":
                             if (currentVALEBuyLists <= 0 && counter > 35) {
                                 to_exchange.println(("ADD 2 VALE BUY " + (Integer.parseInt(reply.substring(14, 18))+1) + " 2").toUpperCase());
-                                System.out.println(reply.substring(15, 19));
+                                System.out.println("Buying VALE @ "+reply.substring(15, 19));
 
                             }
 
@@ -126,11 +140,11 @@ public class Bot
                                     //VALBZ_estimatedValue = (int)reply.substring(15,19);
                                     if (currentGSBuyLists <= 0 && counter > 35 && currentGSSellLists <= 0) {
                                         to_exchange.println(("ADD 3 GS BUY " + ((Integer.parseInt(reply.substring(12, 16))) - 1) + " 2").toUpperCase());
-                                        System.out.println(reply.substring(15, 19));
+                                        System.out.println("Buying GS @ "+reply.substring(15, 19));
                                         for (int x = 0; x < reply.length(); x++) {
                                             if (reply.substring(x, x + 4).equalsIgnoreCase("sell")) {
                                                 to_exchange.println(("ADD 33 GS SELL " + (Integer.parseInt(reply.substring(x + 5, x + 9)) + 1) + " 2").toUpperCase());
-                                                System.out.println(reply.substring(x + 5, x + 9));
+                                                System.out.println("Selling GS @"+reply.substring(x + 5, x + 9));
                                                 break;
                                             }
                                         }
@@ -142,11 +156,11 @@ public class Bot
                                     //VALBZ_estimatedValue = (int)reply.substring(15,19);
                                     if (currentMSBuyLists <= 0 && counter > 35 && currentMSSellLists <= 0) {
                                         to_exchange.println(("ADD 4 MS BUY " + ((Integer.parseInt(reply.substring(12, 16))) - 1) + " 2").toUpperCase());
-                                        System.out.println(reply.substring(15, 19));
+                                        System.out.println("Buying MS @ "+reply.substring(15, 19));
                                         for (int x = 0; x < reply.length(); x++) {
                                             if (reply.substring(x, x + 4).equalsIgnoreCase("sell")) {
                                                 to_exchange.println(("ADD 44 MS SELL " + (Integer.parseInt(reply.substring(x + 5, x + 9))) + " 2").toUpperCase());
-                                                System.out.println(reply.substring(x + 5, x + 9));
+                                                System.out.println("Selling MS @ "+reply.substring(x + 5, x + 9));
                                                 break;
                                             }
                                         }
@@ -158,8 +172,10 @@ public class Bot
 
                             if (currentBondBuyLists <= 0 && counter > 35) {
                                 to_exchange.println(("ADD 7 BOND BUY 999 10").toUpperCase());
+                                System.out.println("Buying BOND @ 999");
                             } else if (currentBondSellLists <= 0 && counter > 35) {
                                 to_exchange.println(("ADD 77 BOND SELL 1001 10").toUpperCase());
+                                System.out.println("Selling BOND @ 1001");
                             }
                             counter++;
                     }
