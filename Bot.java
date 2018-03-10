218b7d2ce6d2684cc24678fc40fe9b318b64e92a
Marco Pretorius <mpre8@student.monash.edu>
2018-03-10 18:12:51 +1100

    Selling Vale

--- Bot.java
+++ Bot.java
@@ -33,10 +33,12 @@ public class Bot
             int currentBondSellLists = 0;
             int currentVALEBuyLists = 0;
             int totalVALE =0;
+            int currentValbz_price;
             int currentGSBuyLists =0;
             int currentGSSellLists =0;
             int currentMSBuyLists =0;
             int currentMSSellLists =0;
+            boolean VALBZ =false;
 
 
                 while ((reply = from_exchange.readLine()) != null) {
@@ -92,7 +94,7 @@ public class Bot
                             if (totalVALE >= 10) {
                                 to_exchange.println(("CONVERT 10 VALE SELL 10").toUpperCase());
                                 System.out.println("Converting Vale");
-
+                                VALBZ =true;
                             }
                             break;
 
@@ -147,6 +149,14 @@ public class Bot
                                 System.out.println("Buying VALE @ "+reply.substring(14, 18));
 
                             }
+
+                            switch (reply.toUpperCase().substring(0, 10)) {
+                                case "BOOK VALBZ":
+                                    if (VALBZ) {
+                                        to_exchange.println(("ADD 2 VALBZ SELL " + (Integer.parseInt(reply.substring(15, 19))) + " 10").toUpperCase());
+                                        System.out.println("Selling VLABZ!!! @ "+reply.substring(15, 19));
+
+                                    }}
 
                             switch (reply.toUpperCase().substring(0, 7)) {
                                 case "BOOK GS":
